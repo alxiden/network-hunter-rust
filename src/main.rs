b9ca1adc77f93f9cccb2139566c91969343dab67
Daniel <github.com.rectify325@passmail.net>
2025-05-28 12:56:38 +0100

    Started looking at iced

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
+use iced::Sandbox;
+use iced::widget::Text;
+use iced::Settings;
+use iced::Element;
+
 struct NetworkHunter {
     ipaddress: String,
     port: u16,
-    
+    scan_type: String,
 }
 
-impl NetworkHunter {
-    // Implement methods for the NetworkHunter struct
+impl Sandbox for NetworkHunter {
+    type Message = ();
+
+    fn new() -> Self {
+        NetworkHunter {
+            
+            ipaddress: String::new(),
+            port: 0,
+            scan_type: String::new(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Network Hunter")
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // Update logic here
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        // View logic here
+        iced::Text::new("Network Hunter").into()
+    }
 }
+
 
 
 fn main() {
-    println!("Hello, world!");
+    NetworkHunter::run(Settings::default()).expect("Failed to run the application");
 }
