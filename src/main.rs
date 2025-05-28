bff31706be8361483aedaf7840777be7a6720d94
Daniel <github.com.rectify325@passmail.net>
2025-05-28 12:23:14 +0100

    Started Project

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+struct NetworkHunter {
+    ipaddress: String,
+    port: u16,
+    
+}
+
+impl NetworkHunter {
+    // Implement methods for the NetworkHunter struct
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
